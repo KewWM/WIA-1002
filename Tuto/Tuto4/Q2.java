beb350e1c8c2ec63b1bbe0c078891bd6a7286fdc
KewWM <weiming050503@gmail.com>
2023-03-28 20:53:40 +0800

    Add Tuto4 finished

---
+++ Tuto/Tuto4/Q2.java
@@ -0,0 +1,39 @@
+
+package Tuto4;
+
+public class Q2 <E>
+{
+//    public boolean contains(E e)
+//    {
+//        Node<E> current = head;
+//
+//        while (current.element != null) 
+//        {
+//
+//            if (current.element == e) 
+//            {
+//                System.out.println(current.element);
+//                return true;
+//            }
+//            current = current.next;
+//        }
+//        return false;
+//    }
+}
+
+/*
+a) The name is contains()
+
+b)
+public void operationX(E e) 
+    {
+        pointerB.next = pointerB;
+        for(int i++; i>size; int i) 
+        {
+            System.out.println(current.element);
+            if(current.element = e)
+        }
+            Node<E> pointerB = head;
+            return false;
+    }
+*/
---
+++ Tuto/Tuto4/Q3.java
@@ -0,0 +1,40 @@
+
+package Tuto4;
+
+public interface Q3 <E>
+{
+    public E removeLast();
+}
+
+/*
+a) The name is removeLast()
+
+b)
+public E removeLast() 
+{
+    if (size == 0) 
+        return null;
+    else if (size == 1) 
+    {
+        size = 0;
+        return tail;
+    }
+    else 
+    {
+        Node<E> pointer1 = head;
+        
+        // Move to last 2nd node
+        for (int i = 1; i < size - 1; i++) 
+            pointer1 = pointer1.next;
+
+        // Remove last node
+        Node<E> temp = tail;
+        tail = pointer1;
+        tail.next = null;
+        
+        size--;
+
+        return temp.element;
+    }
+}
+*/
