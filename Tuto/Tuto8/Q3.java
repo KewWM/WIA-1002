72875dd4e7c50eb258c6ae299caff7f117e3b2be
KewWM <weiming050503@gmail.com>
2023-04-22 23:18:32 +0800

    Lab8 Graph done

---
+++ Tuto/Tuto8/Q3.java
@@ -0,0 +1,40 @@
+
+package Tuto8;
+
+//import java.util.ArrayList;
+
+
+public class Q3 
+{
+    public static void main(String[] args) 
+    {
+//        ArrayList<Vertex> A = new ArrayList<Vertex>();
+//        ArrayList<Vertex> B = new ArrayList<Vertex>();
+//        ArrayList<Vertex> C = new ArrayList<Vertex>();
+//        ArrayList<Vertex> D = new ArrayList<Vertex>();
+//        ArrayList<Vertex> E = new ArrayList<Vertex>();
+//        ArrayList<Vertex> F = new ArrayList<Vertex>();
+//        ArrayList<Vertex> G = new ArrayList<Vertex>();
+//        ArrayList<Vertex> H = new ArrayList<Vertex>();
+//        ArrayList<Vertex> I = new ArrayList<Vertex>();
+//        A.add(A);
+//        A.add(C);
+//        A.add(D);
+//        B.add(B);
+//        B.add(D);
+//        C.add(C);
+//        C.add(E);
+//        C.add(F);
+//        D.add(D);
+//        D.add(E);
+//        E.add(E);
+//        E.add(G);
+//        F.add(F);
+//        F.add(H);
+//        G.add(G);
+//        G.add(H);
+//        H.add(H);
+//        H.add(I);
+//        I.add(I);
+    }
+}
